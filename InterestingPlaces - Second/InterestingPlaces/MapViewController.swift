c59d1c5ea9f898080919f01ab8f38dc507fb1ee0
Elias Gabriel <elias.gabrielfn12@gmail.com>
2019-07-09 11:15:38 -0400

    Getting directions
    Signed-off-by: Elias Gabriel <elias.gabrielfn12@gmail.com>

--- InterestingPlaces - Second/InterestingPlaces/MapViewController.swift
+++ InterestingPlaces - Second/InterestingPlaces/MapViewController.swift
@@ -22,74 +22,114 @@
 
 import UIKit
 import MapKit
+import AVFoundation
 
 class MapViewController: UIViewController {
-
-  @IBOutlet weak var mapView: MKMapView!
-  var place: InterestingPlace?
-  var places: [InterestingPlace] = []
-  var sourcelocation: CLLocation?
-  var travelDirections: [String] = []
-  
-  @IBOutlet weak var tableView: UITableView!
-  override func viewDidLoad() {
+    
+    @IBOutlet weak var mapView: MKMapView!
+    var place: InterestingPlace?
+    var places: [InterestingPlace] = []
+    var sourcelocation: CLLocation?
+    var travelDirections: [String] = []
+    
+    var voice: AVSpeechSynthesizer?
+    
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
         super.viewDidLoad()
-    mapView.delegate = self
-    guard let place = place else { return }
-    let regionRadius: CLLocationDistance = 1000.0
-    let region = MKCoordinateRegion(center: place.location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
-    mapView.setRegion(region, animated: true)
-
-    mapView.addAnnotations(places)
-    mapView.register(InterestingPlaceView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
-    mapView.register(ClusterView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
-    
-    tableView.delegate = self
-    tableView.dataSource = self
-  }
-    
-  @IBAction func close(_ sender: Any) {
-    dismiss(animated: true, completion: nil)
-  }
-  
-  
-  @IBAction func changeMapType(_ sender: UISegmentedControl) {
-    
-    if sender.selectedSegmentIndex == 0 || sender.selectedSegmentIndex == 1 {
-      mapView.isHidden = false
-      tableView.isHidden = true
-      if sender.selectedSegmentIndex == 0 {
-        mapView.mapType = .standard
-      } else if sender.selectedSegmentIndex == 1 {
-        mapView.mapType = .satellite
-      }
-    } else {
-      mapView.isHidden = true
-      tableView.isHidden = false
+        mapView.delegate = self
+        guard let place = place else { return }
+        let regionRadius: CLLocationDistance = 1000.0
+        let region = MKCoordinateRegion(center: place.location.coordinate, latitudinalMeters: regionRadius, longitudinalMeters: regionRadius)
+        
+        voice = AVSpeechSynthesizer()
+        loadDirections()
+        
+        mapView.setRegion(region, animated: true)
+        
+        mapView.addAnnotations(places)
+        mapView.register(InterestingPlaceView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultAnnotationViewReuseIdentifier)
+        mapView.register(ClusterView.self, forAnnotationViewWithReuseIdentifier: MKMapViewDefaultClusterAnnotationViewReuseIdentifier)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
     }
-
-  }
-
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    @IBAction func changeMapType(_ sender: UISegmentedControl) {
+        
+        if sender.selectedSegmentIndex == 0 || sender.selectedSegmentIndex == 1 {
+            mapView.isHidden = false
+            tableView.isHidden = true
+            if sender.selectedSegmentIndex == 0 {
+                mapView.mapType = .standard
+            } else if sender.selectedSegmentIndex == 1 {
+                mapView.mapType = .satellite
+            }
+        } else {
+            mapView.isHidden = true
+            tableView.isHidden = false
+        }
+        
+    }
+    
+    // Carregando directions
+    private func loadDirections() {
+        guard let start = sourcelocation, let end = place else {
+            return
+        }
+        let request = MKDirections.Request()
+        let startMapItem = MKMapItem(placemark: MKPlacemark(coordinate: start.coordinate))
+        let endMapItem = MKMapItem(placemark: MKPlacemark(coordinate: end.coordinate))
+        request.source = startMapItem
+        request.destination = endMapItem
+        request.requestsAlternateRoutes = false
+        request.transportType = .automobile
+        let directions = MKDirections(request: request)
+        directions.calculate { [weak self] (response, error) in
+            if let error = error {
+                print(error.localizedDescription)
+                return
+            }
+            if let route = response?.routes.first {
+                let formatter = MKDistanceFormatter()
+                formatter.unitStyle = .full
+                for step in route.steps {
+                    let distance = formatter.string(fromDistance: step.distance)
+                    self?.travelDirections.append(step.instructions + "\(distance)")
+                }
+            }
+            self?.tableView.reloadData()
+        }
+    }
 }
 
 extension MapViewController: MKMapViewDelegate {
-  
+    
 }
 
 extension MapViewController: UITableViewDelegate {
-  
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let text = travelDirections[indexPath.row]
+        let utterance = AVSpeechUtterance(string: text)
+        voice?.speak(utterance)
+    }
 }
 
 extension MapViewController: UITableViewDataSource {
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return travelDirections.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let cell = tableView.dequeueReusableCell(withIdentifier: "DirectionCell", for: indexPath)
-    cell.textLabel?.text = travelDirections[indexPath.row]
-    return cell
-  }
-  
-  
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return travelDirections.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "DirectionCell", for: indexPath)
+        cell.textLabel?.text = travelDirections[indexPath.row]
+        return cell
+    }
+    
+    
 }
